3b269d0e75268ba006b2a8f05527ac5a9f97c7fc
Paulo Roberto De Morais <prmorais1302@gmail.com>
2026-09-23 13:49:20 -0300

    fix: Changing the scheduled time for sending the email regarding certificate validity

--- src/main/java/br/gov/rn/natal/cadpgmapi/service/email/CertificadoSchedulerService.java
+++ src/main/java/br/gov/rn/natal/cadpgmapi/service/email/CertificadoSchedulerService.java
@@ -21,7 +21,7 @@ public class CertificadoSchedulerService {
 
     // Para teste local imediato (dispara a cada 1 minuto), descomente a linha abaixo e comente a de produção:
 //     @Scheduled(cron = "0 * * * * *")
-     @Scheduled(cron = "0 17 9 * * TUE")
+     @Scheduled(cron = "0 0 8 * * MON")
 
     // Expressão de Produção: Executa toda segunda-feira às 08:00 AM
 //    @Scheduled(cron = "0 0 8 * * MON")
